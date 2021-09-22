bfd8411f10a66d5a59d9331da4b82277129d4433
Oseas Santana <osantana@wiser.com>
2021-09-22 20:25:08 -0300

    Adding docker file and updating argo deployment file

---
+++ infra/run.sh
@@ -0,0 +1,20 @@
+printf '\n### Docker build of simple-api-app started...\n'
+cd ../
+docker build . -t simple-api-app
+printf '\n### Docker build of simple-api-app ended...\n'
+
+printf '\n### Tagging docker image ...\n'
+docker tag simple-api-app oseasjs/simple-api-app:1.0
+
+printf '\n### Pushing image to remote docker repository...\n'
+docker push oseasjs/simple-api-app:1.0
+
+# printf '\n### Starting docker-compose to install services...\n'
+# pwd
+# cd ./infra
+# docker-compose up
+
+# Command to run docker-compose with 3 POC instances
+# docker-compose up --scale app-instance=3
+
+printf '\n### All services were successfully started!'
